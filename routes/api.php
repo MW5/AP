9dc2dfe82e610e3add3acc506b672f1c006fd210
MW5 <mwojcik1988@gmail.com>
2017-07-01 01:47:22 +0200

    tablet api ready

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Resource;
+use App\WarehouseOperation;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +20,46 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/getResourcesApi', function() {
-    $resources = DB::table('resources')->get();
-    echo($resources);
+Route::post('/credentialsApi', function(Request $request) {
+    $name = $request->name;
+    $password = $request->password;
+    
+    if (Auth::attempt(['name' => $name, 'password' => $password])) {
+        echo "true";
+    } else {
+        echo "false";
+    }
 });
 
-Route::post('/credentialsApi', function() {
-    //$users = DB::table('users')->get();
-    echo(true);//testing
+
+//MOVE FUNCTION LOGIC TO CONTROLLERS!!!
+Route::post('/getResources', function(Request $request) {
+    if ($request->token == "token") {
+        $resources = DB::table('resources')->get();
+        echo $resources;
+    } else {
+        echo "false";
+    }
+});
+
+Route::post('/releaseResource', function(Request $request) {
+    if ($request->token == "token") {
+        $resource = Resource::where('name', '=', $request->name)->first();
+        if ($resource->quantity > 0) {
+            $resource->quantity = $resource->quantity-1;
+            $resource->save();
+            
+            $warehouseOperation = new WarehouseOperation();
+            $warehouseOperation->resource_name = $resource->name;
+            $warehouseOperation->operation_type = "wydanie magazynowe";
+            $warehouseOperation->quantity = 1;
+            $warehouseOperation->company_name = "";
+            $warehouseOperation->user_name = $request->userName;
+            $warehouseOperation->save();
+            
+            echo "true";
+        } else {
+            echo "false";
+        }
+    }
 });
